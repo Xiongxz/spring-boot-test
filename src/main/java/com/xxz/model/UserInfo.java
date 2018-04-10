405ae5c42c08cce279edde97144e55366acb63be
superxxzxxz <654962327@qq.com>
2018-04-10 17:29:35 +0800

    地址

--- src/main/java/com/xxz/model/UserInfo.java
+++ src/main/java/com/xxz/model/UserInfo.java
@@ -15,7 +15,7 @@ public class UserInfo {
     private Integer userAge;
 
     private String userName;
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",locale = "zh",timezone = "GMT+8")
     private Date startDate;
 
     public Long getUserId() {
