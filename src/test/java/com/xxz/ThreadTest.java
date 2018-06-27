2bc440aba8982e107fb2493375ea1762e5459636
Xiongxz <654962327@qq.com>
2018-06-27 20:55:20 +0800

    ss

---
+++ src/test/java/com/xxz/ThreadTest.java
@@ -0,0 +1,20 @@
+package com.xxz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/6/27 0027 20:44
+ * @Description:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreadTest {
+    @Test
+    public void threadTest(){
+
+    }
+}
